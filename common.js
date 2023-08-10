b57e06aaf1ffc3ccc67e276be0513a5a4cc7ceed
TuanTV2 Fabbi <tuantv2@fabbi.com.vn>
2023-08-11 00:50:14 +0700

    init project

---
+++ common.js
@@ -0,0 +1,164 @@
+const axios = require("axios");
+const HOST = process.env.HOST_REVERSED;
+const headersDefault = {
+  Accept: "*/*",
+  "Accept-Language": "en-US,en;q=0.9",
+  Connection: "keep-alive",
+  Origin: process.env.ORIGIN,
+  "Sec-Fetch-Dest": "empty",
+  "Sec-Fetch-Mode": "cors",
+  "Sec-Fetch-Site": "same-origin",
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82",
+  "content-type": "application/json",
+  "sec-ch-ua":
+    '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": '"Windows"',
+};
+
+const getData = async (pathString, headers) => {
+  try {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const abortController = new AbortController();
+        const id = setTimeout(() => {
+          abortController.abort();
+          reject({
+            error: "Timeout!",
+          });
+        }, 10000);
+        const pathAndQueryString = pathString.split('?');
+        const path = pathAndQueryString[0];
+        const queryParams = pathAndQueryString[1]?.split('&');
+        const queryStringList = [];
+        queryParams?.map(e => {
+          const queryParamValue = e?.split('=');
+          if (queryParamValue[1]) {
+            const queryTemp = `${queryParamValue[0]}=${encodeURIComponent(queryParamValue[1])}`;
+            queryStringList.push(queryTemp);
+          } else {
+            queryStringList.push(queryParamValue);
+          }
+        });
+        const url = `${HOST}${path}?${queryStringList}`;
+        console.log("url request GET", url);
+        const data = await axios.get(url, {
+          signal: abortController.signal,
+          headers: {
+            ...headersDefault,
+            Authorization: headers.authorization,
+          },
+        });
+        clearTimeout(id);
+        if (data.data) {
+          resolve(data.data);
+        } else {
+          reject(new Error());
+        }
+      } catch (error) {
+        console.log(error);
+        console.log("error", error?.response?.data);
+        if (error.response?.data) {
+          reject(error.response.data);
+        } else {
+          reject({})
+        }
+      }
+    });
+  } catch (error) {
+    console.log("error", error?.messsage);
+  }
+};
+
+
+const postData = async (path, headers, body) => {
+  try {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const abortController = new AbortController();
+        const id = setTimeout(() => {
+          abortController.abort();
+          console.log(1111111111);
+          reject({
+            error: "Timeout!",
+          });
+        }, 20000);
+        const url = `${HOST}${path}`;
+        console.log("url request POST", url);
+        const data = await axios.post(url, body, {
+          signal: abortController.signal,
+          headers: {
+            ...headersDefault,
+            authorization: headers.authorization,
+          },
+        });
+        console.log(data);
+        clearTimeout(id);
+        if (data.data) {
+          resolve(data.data);
+        } else {
+          reject(new Error());
+        }
+      } catch (error) {
+        console.log("error", error.response.data);
+        if (error.response.data) {
+          reject(error.response.data);
+        } else {
+          reject({})
+        }
+      }
+    });
+  } catch (error) {
+    console.log("error");
+  }
+};
+
+
+const putData = async (path, headers, body) => {
+  try {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const abortController = new AbortController();
+        const id = setTimeout(() => {
+          abortController.abort();
+          console.log(1111111111);
+          reject({
+            error: "Timeout!",
+          });
+        }, 20000);
+        const url = `${HOST}${path}`;
+        console.log("url request POST", url);
+        const data = await axios.put(url, body, {
+          signal: abortController.signal,
+          headers: {
+            ...headersDefault,
+            authorization: headers.authorization,
+          },
+        });
+        console.log(data);
+        clearTimeout(id);
+        if (data.data) {
+          resolve(data.data);
+        } else {
+          reject(new Error());
+        }
+      } catch (error) {
+        console.log("error", error.response.data);
+        if (error.response.data) {
+          reject(error.response.data);
+        } else {
+          reject({})
+        }
+      }
+    });
+  } catch (error) {
+    console.log("error");
+  }
+};
+
+module.exports = {
+  getData,
+  postData,
+  putData,
+};
---
+++ server.js
@@ -0,0 +1,52 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+dotenv.config();
+const { getData, postData, putData } = require("./common");
+const axios = require('axios');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    const path = req.query.path;
+    const data = await getData(path, req.headers);
+    return res.json(data);
+  } catch (error) {
+    return res.json({});
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const path = req.query.path;
+    const data = await postData(path, req.headers, req.body);
+    console.log(1111111);
+    return res.send(data)
+  } catch (error) {
+    return res.status(error.code).json(error);
+  }
+});
+
+app.put("/", async (req, res) => {
+  try {
+    const path = req.query.path;
+    const data = await putData(path, req.headers, req.body);
+    console.log(1111111);
+    return res.send(data)
+  } catch (error) {
+    return res.status(error.code).json(error);
+  }
+});
+
+app.listen(process.env.PORT, (err) => {
+  console.log(`Server listening at ${process.env.PORT}`);
+});
+
